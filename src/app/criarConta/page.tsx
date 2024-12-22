dc20e82d0e4f83e9273635d616b8a82d31c3a27e
KAYOGS <kayogabriel.ss16@gmail.com>
2024-12-22 11:18:06 -0300

    ppx 1.0.0

---
+++ src/app/criarConta/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function criarConta() {
+  return (
+    <div className="w-full h-full p-6 flex flex-col justify-between gap-10">
+      criarConta
+    </div>
+  );
+}
---
+++ src/app/recuperarAcesso/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function recuperarAcesso() {
+  return (
+    <div className="w-full h-full p-6 flex flex-col justify-between gap-10">
+      recuperarAcesso
+    </div>
+  );
+}
